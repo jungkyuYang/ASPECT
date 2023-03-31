a298f38b34d51c793e0304286506d333b0347b00
Jungkyu Yang <didwjd8590412@gmail.com>
2023-03-31 23:39:47 +0900

    Keep calm and commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    createDefaultProgram: true,
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
+};
